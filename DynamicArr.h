87a89f1cbd7e21f0c2e6e78f7c1c17a43898d608
igreeda <ii.nikolaev@yandex.ru>
2024-09-30 16:17:34 +0300

    Minor bug fixed

--- DynamicArr.h
+++ DynamicArr.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 
 class DynamicArr {
 	static const size_t initialCapacity_ = 2;	// only one for all objects of DynamicArr
@@ -10,10 +11,12 @@ class DynamicArr {
 
 public:
 	DynamicArr();
-	DynamicArr(size_t size, double element = 0);
+	explicit DynamicArr(size_t size, double element = 0);
 	DynamicArr(const DynamicArr& rhs);
 	~DynamicArr();
 	size_t size() const;
 	size_t capacity() const;
 	void push_back(double elem);
+	double operator[](int index) const;
+	DynamicArr& operator=(const DynamicArr& rhs);
 };

23f68509c43080d215c9bfe6b53188399f2509fb
igreeda <ii.nikolaev@yandex.ru>
2024-11-09 10:38:11 +0300

    HW Compleated

--- main.cpp
+++ main.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
 #include "DynamicArr.h"
-#include "tests.h"
-
 
 int main() {
-	system("chcp 65001");
-	DynamicArr arr1, arr2(3);
-	DynamicArr arr3(arr1);
-	std::cout << arr2.size() << std::endl;
-	for (size_t i = 0; i < 3; ++i) {
-		arr3.push_back(i);
-	}
-	std::cout << arr3[2] << std::endl;
-	arr1 = arr3;
-	std::cout << arr1[0] << std::endl;
-	arr1[0] = 8.2;
-	std::cout << arr1[0] << std::endl;
 
-
-	// test_constructor_default();
-	// test_constructor_with_size();
-	// test_copy_constructor();
-	// test_push_back();
-	// test_assignment_operator();
+	fun();
 	return 0;
 }
